5c60aa44ca4b020bb31fc3640433940f8876d4a4
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-16 12:52:29 -0500

    modify the app to use the redux with the state modules created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import Todo from './components/todo';
+import configureStore from './state/configure-store';
+const store = configureStore();
 
-render(<Todo />, document.getElementById('app'));
+render(
+  <Provider store={store}>
+    <Todo />
+  </Provider>
+, document.getElementById('app'));

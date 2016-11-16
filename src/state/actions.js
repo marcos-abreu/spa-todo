e8609d81c57dee4ade36dd858ddad10d8ba99a21
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-16 14:31:26 -0500

    add delete task action creator

--- src/state/actions.js
+++ src/state/actions.js
@@ -4,12 +4,19 @@ export function addTask(description) {
   return {
     type: types.ADD_TASK,
     description
-  }
+  };
 }
 
 export function toggleTask(id) {
   return {
     type: types.TOGGLE_TASK,
     id
-  }
+  };
+}
+
+export function deleteTask(id) {
+  return {
+    type: types.DELETE_TASK,
+    id
+  };
 }

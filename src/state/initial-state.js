fb56de989d8edbd0370bc8cffe26385c2d4133b2
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-16 12:47:37 -0500

    added initial state of the application

---
+++ src/state/initial-state.js
@@ -0,0 +1,7 @@
+export default {
+  tasks: [
+    { id: 0, description: 'some task', done: false },
+    { id: 1, description: 'another task', done: true },
+    { id: 2, description: 'important task', done: false }
+  ]
+}

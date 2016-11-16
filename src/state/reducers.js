14adb98085863e02d8ce38c831cf58d733f84ac3
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-16 12:49:13 -0500

    added initial reducer structure

---
+++ src/state/reducers.js
@@ -0,0 +1,13 @@
+import initialState from './intial-state';
+
+export default function reducers(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+
+      break;
+
+    default:
+      state;
+
+  }
+}

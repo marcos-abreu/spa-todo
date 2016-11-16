b1367dc97e15c404b4db7fc7f63e816caad7946b
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-16 12:51:23 -0500

    implemented the ADD_TASK reducer

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,14 +1,14 @@
 import * as types from './action-types';
-import initialState from './intial-state';
+import initialState from './initial-state';
 
 export default function reducers(state = initialState, action) {
   switch (action.type) {
     case types.ADD_TASK:
-
-      break;
+      return {
+        tasks: state.tasks.concat([{ id: state.tasks.length, description: action.description, done: false }])
+      }
 
     default:
       state;
-
   }
 }

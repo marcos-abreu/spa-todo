d472be4d273d07af555dee323f65f0d6f150e244
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-17 11:20:23 -0500

    used NODE_ENV to decide when or not to setup Redux DevTools

--- src/state/configure-store.js
+++ src/state/configure-store.js
@@ -4,5 +4,11 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 
 export default function configureStore(state = initialState) {
-  return createStore(reducers, state);
+  if (process.env.NODE_ENV === 'development') {
+    return createStore(reducers, state,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  }
+  else {
+    return createStore(reducers, state);
+  }
 }

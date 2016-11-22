9aef66ae903122ac401d0b9f3432f805318d8eec
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-22 10:33:07 -0500

    implement update profile reducer

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -5,6 +5,7 @@ export default function reducers(state = initialState, action) {
   switch (action.type) {
     case types.DELETE_TASK:
       return {
+        profile: state.profile,
         tasks: state.tasks.filter(function(task) {
           return task.id !== action.id;
         })
@@ -12,6 +13,7 @@ export default function reducers(state = initialState, action) {
 
     case types.TOGGLE_TASK:
       return {
+        profile: state.profile,
         tasks: state.tasks.map(function(task) {
           if (task.id !== action.id) {
             return task;
@@ -25,7 +27,16 @@ export default function reducers(state = initialState, action) {
 
     case types.ADD_TASK:
       return {
+        profile: state.profile,
         tasks: state.tasks.concat([{ id: state.tasks.length, description: action.description, done: false }])
+      };
+
+    case types.UPDATE_PROFILE:
+      const newProfile = Object.assign({}, state.profile);
+      newProfile[action.key] = action.value;
+      return {
+        profile: newProfile,
+        tasks: state.tasks,
       };
 
     default:

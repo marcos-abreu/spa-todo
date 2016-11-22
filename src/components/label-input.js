f1fabcb7732cc9397ce3f45585ce0f30b718a92b
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-22 10:52:05 -0500

    refactor code to remove label/input dupplication

---
+++ src/components/label-input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LabelInput = function(props) {
+  return (
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        id={props.id}
+        type="text"
+        value={props.value}
+        onChange={e => props.update(props.id, e.target.value)} />
+    </div>
+  );
+};
+
+export default LabelInput;
--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from '../state/actions';
+import LabelInput from './label-input';
 
 const UserProfile = function(props) {
 
@@ -12,42 +13,10 @@ const UserProfile = function(props) {
       </header>
 
       <form className="row column">
-        <div>
-          <label htmlFor="first-name">First Name</label>
-          <input
-            id="first-name"
-            type="text"
-            value={props.profile['first-name']}
-            onChange={e => props.updateProfile('first-name', e.target.value)} />
-        </div>
-
-        <div>
-          <label htmlFor="last-name">Last Name</label>
-          <input
-            id="last-name"
-            type="text"
-            value={props.profile['last-name']}
-            onChange={e => props.updateProfile('last-name', e.target.value)} />
-        </div>
-
-        <div>
-          <label htmlFor="phone">Phone Number</label>
-          <input
-            id="phone"
-            type="text"
-            value={props.profile.phone}
-            onChange={e => props.updateProfile('phone', e.target.value)} />
-        </div>
-
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            id="address"
-            type="text"
-            value={props.profile.address}
-            onChange={e => props.updateProfile('address', e.target.value)} />
-        </div>
-
+        <LabelInput id="first-name" label="First Name" update={props.updateProfile} value={props.profile['first-name']} />
+        <LabelInput id="last-name" label="Last Name" update={props.updateProfile} value={props.profile['last-name']} />
+        <LabelInput id="phone" label="Phone Number" update={props.updateProfile} value={props.profile.phone} />
+        <LabelInput id="address" label="Address" update={props.updateProfile} value={props.profile.address} />
         <div>
           <button className="button">Update</button>
         </div>

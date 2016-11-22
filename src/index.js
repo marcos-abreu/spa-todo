cdf3b233d00f1b265ea0e47fcef32457678e4f9a
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-22 10:27:55 -0500

    switch the application to load UserProfile instead of Todo (default)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import Todo from './components/todo';
+import UserProfile from './components/user-profile';
 import configureStore from './state/configure-store';
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Todo />
+    <UserProfile />
   </Provider>
 , document.getElementById('app'));

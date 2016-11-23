bf828b963a4d7d1e9d8a7e0c142596ec834ed0d3
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-23 16:36:11 -0500

    add FETCH_TASKS action type as constant

--- src/state/action-types.js
+++ src/state/action-types.js
@@ -1,6 +1,7 @@
 export const ADD_TASK = 'ADD_TASK';
 export const TOGGLE_TASK = 'TOGGLE_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
+export const FETCH_TASKS = 'FETCH_TASKS';
 
 export const SET_PROFILE_FIELD = 'SET_PROFILE_FIELD';
 export const SAVE_PROFILE = 'SAVE_PROFILE';

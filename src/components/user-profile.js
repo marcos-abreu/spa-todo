1d4f0c305866cb97963d08bbc1070bc92203885c
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-23 16:18:31 -0500

    refactor UserProfile to make use of experimental transform-class-properties

--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -12,13 +12,7 @@ var fields = [
 ];
 
 class UserProfile extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
+  handleClick = e => {
     e.stopPropagation();
     e.preventDefault();
 

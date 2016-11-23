1959292720bc661d7ae7e0d1b78f93778eee33e4
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-23 16:40:34 -0500

    modify the ADD_TASK action intent to execute an async api call before dispatching the action object

--- src/state/actions/task-actions.js
+++ src/state/actions/task-actions.js
@@ -1,9 +1,23 @@
 import * as types from '../action-types';
 
 export function addTask(description) {
-  return {
-    type: types.ADD_TASK,
-    description
+  return function(dispatch, getState) {
+    return fetch('http://localhost:3000/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({description: description})
+    })
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(task) {
+      return dispatch({
+        type: types.ADD_TASK,
+        task
+      });
+    });
   };
 }
 

a2e6150c3e604811c8c645e4ef8b1c13f445f539
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-23 15:31:15 -0500

    configure redux-thunk on our application

--- src/state/configure-store.js
+++ src/state/configure-store.js
@@ -1,8 +1,9 @@
 import initialState from './initial-state';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
 export default function configureStore(state = initialState) {
-  return createStore(reducers, state);
+  return createStore(reducers, state, applyMiddleware(thunk));
 }

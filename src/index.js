7b88181f243ab6421e400c3f81c69edcae013f36
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-25 10:40:55 -0500

    modify app to load a router pointing to the application routes by default

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
 
-import Todo from './components/todo';
+import getRoutes from './routes';
 import configureStore from './state/configure-store';
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Todo />
+    <Router history={hashHistory} routes={getRoutes()} />
   </Provider>
 , document.getElementById('app'));

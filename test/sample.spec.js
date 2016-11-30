0b8047e6afedbbcfa0bf51df6bad02299b84852c
Marcos Abreu <marcos.abreu@capitalone.com>
2016-11-30 09:51:31 -0500

    add sample test to confirm testing configuration

---
+++ test/sample.spec.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('Unit Tests', function() {
+  it('should succeed when given reasonable expectations', function() {
+    expect(true).to.be.true;
+  });
+});
